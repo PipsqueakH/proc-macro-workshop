f18965098811b7c110c506dff6ce7af3fdac4151
Pipsqueak <snipeinmaze@gmail.com>
2020-03-02 11:52:38 +0800

    builder - 02

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,43 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
+use quote::{format_ident, quote};
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    // Parse the input tokens into a syntax tree.
+    let input = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    // Used in the quasi-quotation below as `#name`.
+    let name = input.ident;
+
+    // let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+
+    let builder_name = format_ident!("{}Builder", name);
+
+    let expanded = quote! {
+
+        pub struct #builder_name  {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+
+        // The generated impl.
+        impl  #name  {
+            pub fn builder() -> CommandBuilder {
+                CommandBuilder {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+    };
+
+    // Hand the output tokens back to the compiler.
+    proc_macro::TokenStream::from(expanded)
 }

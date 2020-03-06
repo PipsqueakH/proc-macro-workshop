33e87f2283aa586110e86429af744b5235659fe2
Pipsqueak <snipeinmaze@gmail.com>
2020-03-06 16:08:49 +0800

    rectify builder 02

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,9 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
-use quote::{format_ident, quote};
-use syn::{parse_macro_input, DeriveInput};
+use quote::{format_ident, quote, quote_spanned};
+use syn::spanned::Spanned;
+use syn::{parse_macro_input, Data, DeriveInput, Fields};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -15,76 +16,61 @@ pub fn derive(input: TokenStream) -> TokenStream {
     // let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
 
     let builder_name = format_ident!("{}Builder", name);
+    let (name_vec, type_vec): (Vec<proc_macro2::TokenStream>, Vec<proc_macro2::TokenStream>) =
+        match input.data {
+            Data::Struct(ref data) => match data.fields {
+                Fields::Named(ref fields) => fields
+                    .named
+                    .iter()
+                    .map(|f| {
+                        let n = f.ident.as_ref();
+                        let t = &f.ty;
+                        (quote! {#n}, quote! {#t})
+                    })
+                    .unzip(),
+                Fields::Unnamed(_) | Fields::Unit => unimplemented!(),
+            },
+            Data::Enum(_) | Data::Union(_) => unimplemented!(),
+        };
+
+    // let field_name = name_iter.collect();
+    // eprintln!("TOKENS: {}", name_iter[0]);
+    // eprintln!("TOKENS: {}", field_type);
 
     let expanded = quote! {
 
         use std::error::Error;
 
-        pub struct #builder_name  {
-            executable: Option<String>,
-            args: Option<Vec<String>>,
-            env: Option<Vec<String>>,
-            current_dir: Option<String>,
-        }
+        pub struct #builder_name  { #( #name_vec : Option< #type_vec>,)*}
 
         // The generated impl.
         impl  #name  {
-            pub fn builder() -> CommandBuilder {
-                CommandBuilder {
-                    executable: None,
-                    args: None,
-                    env: None,
-                    current_dir: None,
-                }
+            pub fn builder() -> #builder_name {
+                #builder_name { #( #name_vec : None,)*}
             }
         }
 
-        impl #builder_name {
-            fn executable(&mut self, executable: String) -> &mut Self {
-                self.executable = Some(executable);
-                self
-            }
-        }
 
-        impl #builder_name {
-            fn args(&mut self, args: Vec<String>) -> &mut Self {
-                self.args = Some(args);
-                self
-            }
-        }
 
-        impl #builder_name {
-            fn env(&mut self, env: Vec<String>) -> &mut Self {
-                self.env = Some(env);
-                self
-            }
-        }
+        // impl #builder_name {
+        //     pub fn build(&mut self) -> Result<Command, Box<dyn Error>> {
+        //         let ex = self.executable.clone().ok_or_else::<Box<dyn Error>, _>(||::std::convert::From::from("has no executable"))?;
+        //         let ar = self.args.clone().ok_or_else::<Box<dyn Error>, _>(||::std::convert::From::from("has no args"))?;
+        //         let en = self.env.clone().ok_or_else::<Box<dyn Error>, _>(||::std::convert::From::from("has no enc"))?;
+        //         let cd = self.current_dir.clone().ok_or_else::<Box<dyn Error>, _>(||::std::convert::From::from("has no current_dir"))?;
 
-        impl #builder_name {
-            fn current_dir(&mut self, current_dir: String) -> &mut Self {
-                self.current_dir = Some(current_dir);
-                self
-            }
-        }
-
-        impl #builder_name {
-            pub fn build(&mut self) -> Result<Command, Box<dyn Error>> {
-                let ex = self.executable.clone().ok_or_else::<Box<dyn Error>, _>(||::std::convert::From::from("has no executable"))?;
-                let ar = self.args.clone().ok_or_else::<Box<dyn Error>, _>(||::std::convert::From::from("has no args"))?;
-                let en = self.env.clone().ok_or_else::<Box<dyn Error>, _>(||::std::convert::From::from("has no enc"))?;
-                let cd = self.current_dir.clone().ok_or_else::<Box<dyn Error>, _>(||::std::convert::From::from("has no current_dir"))?;
-
-                Ok(Command {
-                    executable: ex,
-                    args: ar,
-                    env: en,
-                    current_dir: cd,
-                })
-            }
-        }
+        //         Ok(Command {
+        //             executable: ex,
+        //             args: ar,
+        //             env: en,
+        //             current_dir: cd,
+        //         })
+        //     }
+        // }
 
     };
 
+    eprintln!("TOKENS: {}", expanded);
     // Hand the output tokens back to the compiler.
     proc_macro::TokenStream::from(expanded)
 }

1664e48759f557647d9d09bf9780c7a602c9919e
Pipsqueak <snipeinmaze@gmail.com>
2020-03-08 12:55:36 +0800

    builder refactor 1-5

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -3,7 +3,10 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 use quote::{format_ident, quote, quote_spanned};
 use syn::spanned::Spanned;
-use syn::{parse_macro_input, Data, DeriveInput, Fields};
+use syn::{
+    parse_macro_input, AngleBracketedGenericArguments, Data, DataStruct, DeriveInput, Fields,
+    FieldsNamed, GenericArgument, Path, PathArguments, Type, TypePath,
+};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -14,45 +17,65 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let name = input.ident;
     let builder_name = format_ident!("{}Builder", name);
 
-    let (name_vec, type_vec): (Vec<proc_macro2::TokenStream>, Vec<proc_macro2::TokenStream>) =
-        match input.data {
-            Data::Struct(ref data) => match data.fields {
-                Fields::Named(ref fields) => fields
-                    .named
-                    .iter()
-                    .map(|f| {
-                        let n = f.ident.as_ref();
-                        let t = &f.ty;
-                        (quote! {#n}, quote! {#t})
-                    })
-                    .unzip(),
-                Fields::Unnamed(_) | Fields::Unit => unimplemented!(),
-            },
-            Data::Enum(_) | Data::Union(_) => unimplemented!(),
-        };
+    let origin_fields = if let Data::Struct(DataStruct {
+        fields: Fields::Named(FieldsNamed { ref named, .. }, ..),
+        ..
+    }) = input.data
+    {
+        named
+    } else {
+        unimplemented! {}
+    };
+
+    let builder_fields = origin_fields.iter().map(|x| {
+        let n = x.ident.as_ref();
+        let t = &x.ty;
+        quote! { #n: Option< #t>}
+    });
+
+    let builder_fields_name = origin_fields.iter().map(|x| {
+        let n = x.ident.as_ref();
+        quote! { #n}
+    });
+
+    let setter_method = origin_fields.iter().map(|x| {
+        let n = x.ident.as_ref();
+        let t = &x.ty;
+        quote! {
+            fn #n(&mut self, #n: #t) -> &mut Self {
+                self.#n = Some(#n);
+                self
+            }
+        }
+    });
+
+    let built_fields = origin_fields.iter().map(|x| {
+        let n = x.ident.as_ref();
+        let err_msg = concat!(stringify!(n));
+        quote! {
+            #n: self.#n.clone().ok_or_else::<Box<dyn Error>, _>(||::std::convert::From::from(#err_msg))?
+        }
+    });
 
     // let field_name = name_iter.collect();
     // eprintln!("TOKENS: {}", name_iter[0]);
-    // eprintln!("TOKENS: {}", field_type);
+    // eprintln!("TOKENS: {:?}", builder_fields_name);
 
     let expanded = quote! {
 
         use std::error::Error;
 
-        pub struct #builder_name  { #( #name_vec : Option< #type_vec>,)*}
+        pub struct #builder_name  { #( #builder_fields ,)*}
 
         // The generated impl.
         impl  #name  {
             pub fn builder() -> #builder_name {
-                #builder_name { #( #name_vec : None,)*}
+                #builder_name { #( #builder_fields_name : None,)*}
             }
         }
 
         impl #builder_name {
-            #(fn #name_vec(&mut self, #name_vec: #type_vec) -> &mut Self {
-                self.#name_vec = Some(#name_vec);
-                self
-            })*
+            #(#setter_method)*
 
         }
 
@@ -60,8 +83,8 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
         impl #builder_name {
             pub fn build(&mut self) -> Result<#name, Box<dyn Error>> {
-                #(let #name_vec = self.#name_vec.clone().ok_or_else::<Box<dyn Error>, _>(||::std::convert::From::from("there is missing field"))?;)*
-                Ok(Command {#(#name_vec ,)*})
+
+                Ok(Command {#(#built_fields ,)*})
             }
         }
 

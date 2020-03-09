00b6bf104242cede6d5497498a58d62e3ee9abc9
Pipsqueak <snipeinmaze@gmail.com>
2020-03-09 20:24:06 +0800

    builder 08

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -2,12 +2,11 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use proc_macro2::{Ident, Span};
-use quote::{format_ident, quote, quote_spanned};
-use syn::spanned::Spanned;
+use quote::{format_ident, quote};
 use syn::{
-    parse_macro_input, AngleBracketedGenericArguments, Attribute, Data, DataStruct, DeriveInput,
-    Fields, FieldsNamed, GenericArgument, Lit, LitStr, Meta, MetaList, MetaNameValue, NestedMeta,
-    Path, PathArguments, Type, TypePath,
+    parse_macro_input, spanned::Spanned, AngleBracketedGenericArguments, Attribute, Data,
+    DataStruct, DeriveInput, Error, Fields, FieldsNamed, GenericArgument, Lit, Meta, MetaList,
+    MetaNameValue, NestedMeta, PathArguments, Result, Type, TypePath,
 };
 
 #[proc_macro_derive(Builder, attributes(builder))]
@@ -29,12 +28,20 @@ pub fn derive(input: TokenStream) -> TokenStream {
         unimplemented! {}
     };
 
+    // let mut err_code = proc_macro2::TokenStream::new();
+
+    // eprintln! {"{:?}", err_code};
+
     let builder_fields = origin_fields.iter().map(|x| {
         let n = x.ident.as_ref();
         let ty = &x.ty;
         if x.attrs.len() != 0 {
+            // if let Err(e) = setter_name(&x.attrs[0]) {
+            //     err_code = e.to_compile_error();
+            //     println!("err code is{:?}", err_code);
+            // }
             let inner_ty = type_in_container("Vec", ty);
-            if setter_name(&x.attrs[0]).is_some() && inner_ty.is_some() {
+            if inner_ty.is_some() {
                 return quote! { #n:  #ty};
             }
         }
@@ -48,7 +55,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
         let ty = &x.ty;
         if x.attrs.len() != 0 {
             let inner_ty = type_in_container("Vec", ty);
-            if setter_name(&x.attrs[0]).is_some() && inner_ty.is_some() {
+            if inner_ty.is_some() {
                 return quote! { #n:  Vec::new()};
             }
         }
@@ -58,13 +65,13 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let setter_method = origin_fields.iter().map(|x| {
         let n = x.ident.as_ref();
 
-        // println! {" attts: {:?}", x.attrs};
+        println! {" attts: {:?}", x.attrs};
         if x.attrs.len() != 0 {
             let setter_name = setter_name(&x.attrs[0]);
-            // println! {" got attr: {:?}", setter_name};
+            println! {" got attr: {:?}", setter_name};
             let veced_ty = type_in_container("Vec", &x.ty);
-            if setter_name.is_some() && veced_ty.is_some() {
-                let sn = setter_name.unwrap();
+            if setter_name.is_ok() && veced_ty.is_some() {
+                let sn = setter_name.ok().unwrap();
                 // eprintln!("setter name is {:?}", sn);
 
                 return quote! {
@@ -73,6 +80,10 @@ pub fn derive(input: TokenStream) -> TokenStream {
                         self
                     }
                 };
+            } else {
+                if let Err(e) = setter_name {
+                    return e.to_compile_error();
+                }
             }
         }
 
@@ -95,7 +106,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
             }
         } else {
             quote! {
-            #n: self.#n.clone().ok_or_else::<Box<dyn Error>, _>(||::std::convert::From::from(#err_msg))?
+            #n: self.#n.clone().ok_or_else::<Box<dyn std::error::Error>, _>(||::std::convert::From::from(#err_msg))?
             }
         }
         
@@ -107,9 +118,12 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     let expanded = quote! {
 
-        use std::error::Error;
+
+        // use std::error::Error;
 
         pub struct #builder_name  { #( #builder_fields ,)*}
+
+        // #err_code;
 
         // The generated impl.
         impl  #name  {
@@ -126,7 +140,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
 
         impl #builder_name {
-            pub fn build(&mut self) -> Result<#name, Box<dyn Error>> {
+            pub fn build(&mut self) -> Result<#name, Box<dyn std::error::Error>> {
 
                 Ok(Command {#(#built_fields ,)*})
             }
@@ -157,7 +171,7 @@ fn type_in_container<'a>(container: &str, ty: &'a Type) -> Option<&'a Type> {
     None
 }
 
-fn setter_name(attr: &Attribute) -> Option<Ident> {
+fn setter_name(attr: &Attribute) -> Result<Ident> {
     match attr.parse_meta() {
         Ok(ref meta) => {
             // println!("meta: {:?}", meta);
@@ -173,8 +187,16 @@ fn setter_name(attr: &Attribute) -> Option<Ident> {
                         let attr_name = path.segments.first().unwrap();
                         if attr_name.ident == "each" {
                             if let Lit::Str(ref s) = lit {
-                                return Some(Ident::new(&s.value(), Span::call_site()));
+                                return Ok(Ident::new(&s.value(), s.span()));
                             }
+                        } else {
+                            // compile_error!("aaa");
+                            // println!(" ident is {:?}", attr_name.ident);
+
+                            return Err(Error::new(
+                                meta.span(),
+                                "expected `builder(each = \"...\")`",
+                            ));
                         }
                     }
                 }
@@ -182,9 +204,9 @@ fn setter_name(attr: &Attribute) -> Option<Ident> {
         }
         Err(err) => {
             println!("parse setter name err: {:?}", err);
-            return None;
+            return Err(Error::new(Span::call_site(), "parse attribute error"));
         }
     }
 
-    None
+    return Err(Error::new(Span::call_site(), "parse attribute error"));
 }
